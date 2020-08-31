d76d4ab5a5a0ef6cc1b0d20ba417b0891edc5efc
Stu Hanford <shanford3@gmail.com>
2020-08-31 12:43:14 -0700

    Implement all features asked

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
 var App = () => (
   <div>
     <h2>My Todo List</h2>
-    <GroceryList />
+    <GroceryList todos={['Apples', 'Eggs', 'Bread']} />
   </div>
 );
 
-// var TodoList = (props) => (
-
+// var Apples = (props) => (
+//   <li>Apples</li>
 // );
 
-var Apples = (props) => (
-  <li>Apples</li>
-);
-
-var Eggs = (props) => (
-  <li>Eggs</li>
-);
+// var Eggs = (props) => (
+//   <li>Eggs</li>
+// );
 
 var GroceryList = (props) => (
-  <div>
-    <ul>
-      <Apples />
-      <Eggs />
-    </ul>
-  </div>
+  <ul>{props.todos.map(todo =>
+    <GroceryListItem todo={todo} />
+  )}
+  </ul>
 );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      crossed: false,
+      hovered: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      crossed: !this.state.crossed
+    });
+  }
+
+  onListItemHover() {
+    this.setState({
+      hovered: !this.state.hovered
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.crossed ? 'line-through' : 'none',
+      fontWeight: this.state.hovered ? 'bold' : 'normal'
+    };
+    return (
+      <li
+        style={style}
+        onMouseEnter={this.onListItemHover.bind(this)}
+        onMouseLeave={this.onListItemHover.bind(this)}
+        onClick={this.onListItemClick.bind(this)}>
+        {this.props.todo}
+      </li>
+    )
+  }
+};
+
+
 
 ReactDOM.render(<App />, document.getElementById("app"));
