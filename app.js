e79eaaece365811ba02f1a0a910f97de00bc3899
Stu Hanford <shanford3@gmail.com>
2020-08-31 12:24:46 -0700

    Set up GroceryList and components for items on list

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <GroceryList />
+  </div>
+);
+
+// var TodoList = (props) => (
+
+// );
+
+var Apples = (props) => (
+  <li>Apples</li>
+);
+
+var Eggs = (props) => (
+  <li>Eggs</li>
+);
+
+var GroceryList = (props) => (
+  <div>
+    <ul>
+      <Apples />
+      <Eggs />
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
